390a16aefd95f788e143a9c231c9813f7f2739d8
Zhang <jiez.zhang@intel.com>
2019-12-05 13:58:38 +0800

    Update to hiera-parallel way to submit kernel

--- src/CLSmith/SYCLSmith/sycl_launcher.cpp
+++ src/CLSmith/SYCLSmith/sycl_launcher.cpp
@@ -9,20 +9,26 @@ int main(int argc, char **argv) {
   std::string device_name = arg.get_required_value("d");
   fuzz_selector selector(platform_name, device_name);
 
-  const int dim = 10000;
+  const int dim = 1024;
+  const int local_dim = 64;
   std::vector<int> result(dim);
 
   {
     cl::sycl::queue queue(selector);
-    cl::sycl::range<1> range(dim);
+    cl::sycl::range<1> global_range(dim);
+    cl::sycl::range<1> local_range(local_dim);
     cl::sycl::buffer<int, 1> res_buffer(result.data(), range);
 
     queue.submit([&](cl::sycl::handler &cgh) {
       auto acc =
-          res_buffer.template get_access<cl::sycl::access::mode::read_write>(cgh);
-      cgh.parallel_for<class atomic_size_t>(range, [=](cl::sycl::item<1> item) {
-
-      });
+          res_buffer.template get_access<cl::sycl::access::mode::read_write>(
+              cgh);
+      cgh.parallel_for_work_group<class kernel>(
+          global_range, local_range, [=](cl::sycl::group<1> group) {
+            group.parallel_for_work_item([&](cl::sycl::h_item<1> item) {
+              kernel(item, acc);
+            });
+          });
     });
   }
 }
