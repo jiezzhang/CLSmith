0ce1473293676e8b76ed33df1ae7ced35f56fa66
Zhang <jiez.zhang@intel.com>
2019-12-25 13:23:08 +0800

    Add compiling option for different kernel invoker

--- src/CLSmith/SYCLSmith/sycl_launcher.cpp
+++ src/CLSmith/SYCLSmith/sycl_launcher.cpp
@@ -20,6 +20,24 @@ int main(int argc, char **argv) {
     cl::sycl::range<1> local_range(local_dim);
     cl::sycl::buffer<ulong, 1> res_buffer(result.data(), global_range);
 
+#if defined(SINGLE_TASK)
+    queue.submit([&](cl::sycl::handler &cgh) {
+      auto acc =
+          res_buffer.template get_access<cl::sycl::access::mode::read_write>(
+              cgh);
+      cgh.single_task<class fuzz_kernel>([=]() {
+        kernel(global_range, acc);
+      }); // Input range to prevent out-of-bound
+    });
+#elif defined(PARALLEL_FOR_RANGE)
+    queue.submit([&](cl::sycl::handler &cgh) {
+      auto acc =
+          res_buffer.template get_access<cl::sycl::access::mode::read_write>(
+              cgh);
+      cgh.parallel_for<class fuzz_kernel>(
+          global_range, [=](cl::sycl::item<1> item) { kernel(item, acc); });
+    });
+#elif defined(PARALLEL_FOR_ND_RANGE)
     queue.submit([&](cl::sycl::handler &cgh) {
       auto acc =
           res_buffer.template get_access<cl::sycl::access::mode::read_write>(
@@ -28,5 +46,31 @@ int main(int argc, char **argv) {
           cl::sycl::nd_range<1>(global_range, local_range),
           [=](cl::sycl::nd_item<1> item) { kernel(item, acc); });
     });
+#elif defined(PARALLEL_FOR_GROUP)
+    queue.submit([&](cl::sycl::handler &cgh) {
+      auto acc =
+          res_buffer.template get_access<cl::sycl::access::mode::read_write>(
+              cgh);
+      cgh.parallel_for_work_group<class fuzz_kernel>(
+          global_range, local_range, [=](cl::sycl::group<1> group) {
+            group.parallel_for_work_item(
+                [&] cl::sycl::h_item<1> item { kernel(item, acc); });
+          });
+    });
+#else
+    std::cout << "Invalid kernel submitting way" << std::endl;
+    return 1;
+#endif
   }
+
+#ifndef SINGLE_TASK
+  // Data verification
+  ulong baseline = result[0];
+  for (size_t i = 0; i < dim; ++i) {
+    if (result[i] != baseline) {
+      printf("Result mismatch in %zu: %lu vs %lu \n", i, result[i], baseline);
+    }
+  }
+#endif
+  return 0;
 }
